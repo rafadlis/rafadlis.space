d475f848a70b310b2dbe043bca6dab6fb743ab0d
rafadlis <rahmatfadlis@gmail.com>
2024-11-17 17:04:37 +0700

    feat: add `loading.tsx` in root

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-dvh p-4 flex justify-center items-center">
+      <div className="w-10 h-10 rounded-full border-4 border-t-transparent border-b-transparent border-r-primary animate-spin"></div>
+    </div>
+  );
+}
