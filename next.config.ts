3c6600c2551a886aeb97a13aa1d906c540b808b6
rafadlis <rahmatfadlis@gmail.com>
2024-11-17 17:04:02 +0700

    fix: static export not support `dynamicIO` and `ppr`.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  experimental: {
-    dynamicIO: true,
-    ppr: true,
-  },
 };
 
 export default nextConfig;
