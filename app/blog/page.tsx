58ad7ef884e6de515070e5903b1d0c2bed00c737
rafadlis <rahmatfadlis@gmail.com>
2025-06-14 19:50:17 +0700

    chore(blog): remove unnecessary semicolon in blog page component for cleaner code

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
-import { ComingSoon } from "@/components/coming-soon";
+import { ComingSoon } from "@/components/coming-soon"
 
 export default function BlogPage() {
-  return <ComingSoon />;
+  return <ComingSoon />
 }
