84122fdc40e38093f7611e173b0cfa712cf0e089
rafadlis <rahmatfadlis@gmail.com>
2025-06-14 19:49:37 +0700

    feat(blog): add authors module with author types and sample data for improved content management

---
+++ lib/authors.ts
@@ -0,0 +1,37 @@
+import { GithubLogo, InstagramLogo } from "@phosphor-icons/react/dist/ssr"
+import { type Icon } from "@phosphor-icons/react"
+
+export type Author = {
+  name: string
+  image: string
+  url?: string
+  socials?: {
+    name: string
+    icon: Icon
+    url: string
+  }[]
+}
+
+export type PostAuthor = Author & {
+  role?: string
+}
+
+export const authors: Record<string, Author> = {
+  rafadlis: {
+    name: "R. Rahmat Fadli Sadikin",
+    image: "https://github.com/rafadlis.png",
+    url: "https://rafadlis.space",
+    socials: [
+      {
+        name: "GitHub",
+        icon: GithubLogo,
+        url: "https://github.com/rafadlis",
+      },
+      {
+        name: "Instagram",
+        icon: InstagramLogo,
+        url: "https://instagram.com/rafadlis",
+      },
+    ],
+  },
+}
