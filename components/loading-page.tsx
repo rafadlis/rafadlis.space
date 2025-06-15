bef8dd93a6e7dc0015dbff9ab77629cf3f53d51a
rafadlis <rahmatfadlis@gmail.com>
2025-06-15 13:06:00 +0700

    feat: add LoadingPage component using animation

---
+++ components/loading-page.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils"
+import RDotIconLoading from "./r-dot-icon-loading"
+
+export function LoadingPage({ fullPage = false }: { fullPage?: boolean }) {
+  return (
+    <section
+      className={cn(
+        "flex items-center justify-center h-full",
+        fullPage && "h-svh"
+      )}
+    >
+      <RDotIconLoading className="size-10" />
+    </section>
+  )
+}
