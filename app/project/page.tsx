95931e098f84f608363e7a5a46cc9f330e78923c
rafadlis <rahmatfadlis@gmail.com>
2025-07-15 20:34:28 +0700

    feat(project): add loading component and enhance project page layout with icons and navigation

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,28 +1,66 @@
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { ProjectTable } from "../_components/project-table"
+import {
+  Browser,
+  Code,
+  Globe,
+  HouseSimpleIcon,
+} from "@phosphor-icons/react/dist/ssr"
+import { ProjectTable } from "./_components/project-table"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function ProjectPage() {
   return (
-    <article className="min-h-dvh p-4 flex flex-col gap-4 relative">
+    <article className="min-h-dvh p-4 space-y-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>About Projects</CardTitle>
+          <CardDescription>
+            Here are some of the projects I&apos;ve worked on.
+          </CardDescription>
+          <CardAction>
+            <Button asChild variant="outline">
+              <Link href="/">
+                <HouseSimpleIcon />
+                <span className="max-md:hidden">Back to Home</span>
+              </Link>
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-2 gap-4 py-4 sm:grid-cols-3 lg:grid-cols-5">
+            <div className="flex flex-col items-center gap-2">
+              <Browser className="text-3xl" weight="light" />
+              <p className="text-sm">Web App</p>
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <Globe className="text-3xl" weight="light" />
+              <p className="text-sm">Website</p>
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <Code className="text-3xl" weight="light" />
+              <p className="text-sm">Web API</p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
       <Card>
         <CardHeader>
           <CardTitle>Projects</CardTitle>
-          <CardDescription>The projects that I have worked on</CardDescription>
+          <CardDescription>
+            Here are some of the projects I&apos;ve worked on.
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <ProjectTable />
         </CardContent>
-        <CardFooter className="text-sm text-muted-foreground">
-          This not all my projects, just the ones that I&apos;m prepared well
-          enough to show
-        </CardFooter>
       </Card>
     </article>
   )
