68cf199148b277f8ecfc3acfc52e839ff5214830
rafadlis <rahmatfadlis@gmail.com>
2025-07-15 20:32:33 +0700

    refactor(data-project): update project structure by replacing id with slug and simplifying category representation

--- lib/data-project.ts
+++ lib/data-project.ts
@@ -6,10 +6,10 @@ export type ProjectStatus =
   | "dropped"
   | "growing"
 
-export type ProjectCategory = "web-app" | "website" | "api-web"
+export type ProjectCategory = "Web App" | "Website" | "Web API"
 
 export type Project = {
-  id: number
+  slug: string
   name: string
   urls?: {
     prod?: string
@@ -19,10 +19,7 @@ export type Project = {
   isPrivate: boolean
   isOpenSource: boolean
   description: string
-  category: {
-    name: string
-    icon: ProjectCategory
-  }
+  category: ProjectCategory
   tech: Array<{
     name: string
     version?: string
@@ -33,17 +30,14 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    id: 7,
+    slug: "mauscan-online",
     name: "mauscan.online",
     version: "v0.0.0-alpha",
     isPrivate: false,
     isOpenSource: false,
     description:
       "A powerful API for scanning and managing specific documents like Identity Cards, featuring reliable AI-powered OCR for a cost-effective and streamlined workflow. It is complemented by a web application for easy access and integration.",
-    category: {
-      name: "Web App",
-      icon: "web-app",
-    },
+    category: "Web App",
     tech: [
       { name: "Next.js 15", version: "15^", purpose: "Framework" },
       { name: "TypeScript", version: "5^", purpose: "Language" },
@@ -56,7 +50,7 @@ export const projects: Project[] = [
     status: "in-progress",
   },
   {
-    id: 6,
+    slug: "tirtakencana-co-id",
     name: "tirtakencana.co.id",
     urls: {
       prod: "https://tirtakencana.co.id",
@@ -66,10 +60,7 @@ export const projects: Project[] = [
     isOpenSource: false,
     description:
       "Primary website for Tirtakencana as a hotel and resort company.",
-    category: {
-      name: "Website",
-      icon: "website",
-    },
+    category: "Website",
     tech: [
       { name: "Next.js 15", version: "15^", purpose: "Framework" },
       { name: "TypeScript", version: "5^", purpose: "Language" },
@@ -81,7 +72,7 @@ export const projects: Project[] = [
     status: "in-progress",
   },
   {
-    id: 5,
+    slug: "tirtakencana-app",
     name: "app.tirtakencana.co.id",
     urls: {
       prod: "https://app.tirtakencana.co.id",
@@ -92,10 +83,7 @@ export const projects: Project[] = [
     isOpenSource: false,
     description:
       "A comprehensive Hotel and Resort Management System designed to streamline operations from Front Office to Accounting. Features include reservation management, guest services, room allocation, billing, and financial reporting.",
-    category: {
-      name: "Web App",
-      icon: "web-app",
-    },
+    category: "Web App",
     tech: [
       { name: "Next.js 15", version: "15^", purpose: "Framework" },
       { name: "TypeScript", version: "5^", purpose: "Language" },
@@ -109,7 +97,7 @@ export const projects: Project[] = [
     status: "growing",
   },
   {
-    id: 4,
+    slug: "sapada",
     name: "sapada.bapenda.garutkab.go.id",
     urls: {
       prod: "https://sapada.bapenda.garutkab.go.id",
@@ -120,10 +108,7 @@ export const projects: Project[] = [
     isOpenSource: false,
     description:
       "SAPADA (Regional Tax Administration System) is a comprehensive web platform designed to modernize and streamline regional tax management. It provides efficient tools for tax collection, administration, and reporting, helping government agencies manage their tax operations more effectively.",
-    category: {
-      name: "Web App",
-      icon: "web-app",
-    },
+    category: "Web App",
     tech: [
       { name: "Next.js 15", version: "15.0.0", purpose: "Framework" },
       { name: "TypeScript", version: "5.0.0", purpose: "Language" },
@@ -136,17 +121,14 @@ export const projects: Project[] = [
     status: "in-progress",
   },
   {
-    id: 3,
+    slug: "apeda",
     name: "APEDA",
     version: "v1.0.3",
     isPrivate: true,
     isOpenSource: false,
     description:
       "APEDA (AP3DA - Aplikasi Pengawasan dan Pemeriksaan Pajak Daerah)  A web application for efficient tax supervision and inspection management, streamlining administrative workflows and compliance monitoring.",
-    category: {
-      name: "Web App",
-      icon: "web-app",
-    },
+    category: "Web App",
     tech: [
       { name: "Next.js 15", version: "15.0.0", purpose: "Framework" },
       { name: "TypeScript", version: "5.0.0", purpose: "Language" },
@@ -158,17 +140,14 @@ export const projects: Project[] = [
     status: "growing",
   },
   {
-    id: 2,
+    slug: "rafadlis-space",
     name: "rafadlis.space",
     version: "v0.0.0-beta",
     isPrivate: false,
     isOpenSource: true,
     description:
       "A dynamic digital portfolio showcasing my journey as a developer, featuring interactive project demonstrations, technical expertise, and creative solutions that bring ideas to life.",
-    category: {
-      name: "Website",
-      icon: "website",
-    },
+    category: "Website",
     tech: [
       { name: "Next.js 15", version: "15.0.0", purpose: "Framework" },
       { name: "TypeScript", version: "5.0.0", purpose: "Language" },
@@ -178,17 +157,14 @@ export const projects: Project[] = [
     status: "growing",
   },
   {
-    id: 1,
+    slug: "epad-api",
     name: "EPAD API",
     version: "v2.0.0",
     isPrivate: true,
     isOpenSource: false,
     description:
       "A comprehensive RESTful API service for e-PAD (Elektronik Pendataan Daerah), providing secure and efficient data management, real-time synchronization, and seamless integration capabilities for government applications. Built with scalability and performance in mind.",
-    category: {
-      name: "API Web",
-      icon: "api-web",
-    },
+    category: "Web API",
     tech: [
       { name: "Next.js 15", version: "15^", purpose: "Framework" },
       { name: "TypeScript", version: "5^", purpose: "Language" },
