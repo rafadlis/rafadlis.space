eef0cfa362b92995dae7e2e3733746085e84c3a6
rafadlis <rahmatfadlis@gmail.com>
2025-07-17 16:18:30 +0700

    fix(blog): update background color for code snippets in blog component for improved visibility

--- app/blog/_components/blog.tsx
+++ app/blog/_components/blog.tsx
@@ -384,7 +384,7 @@ function BlogCode({ className, ...props }: React.ComponentProps<"code">) {
   return (
     <code
       className={cn(
-        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        "relative rounded bg-muted-foreground/15 px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
         className
       )}
       {...props}
