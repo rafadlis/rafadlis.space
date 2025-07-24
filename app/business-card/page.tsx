24cb697e5e62be06238e84bf87ad9c17dcca792e
rafadlis <rahmatfadlis@gmail.com>
2025-07-24 09:45:53 +0700

    fix(business-card): adjust print styles for business card layout

--- app/business-card/page.tsx
+++ app/business-card/page.tsx
@@ -1,10 +1,10 @@
 export default function BusinessCard() {
   return (
     <article className="flex flex-row flex-wrap gap-6 h-screen w-full items-center justify-center *:rounded-md *:print:rounded-none px-6 py-12">
-      <div className="bg-foreground flex h-[51mm] w-[89mm] flex-col items-center justify-center shadow-2xl">
+      <div className="bg-foreground flex h-[51mm] w-[89mm] flex-col items-center justify-center shadow-2xl print:shadow-none">
         <h1 className="text-5xl font-medium text-background">r.</h1>
       </div>
-      <div className="h-[51mm] w-[89mm] shadow-2xl py-4 px-6 grid grid-cols-2 grid-rows-2">
+      <div className="h-[51mm] w-[89mm] shadow-2xl print:shadow-none py-4 px-6 grid grid-cols-2 grid-rows-2">
         <div>
           <h1 className="text-xs">
             me@rafadlis.space / https://rafadlis.space
