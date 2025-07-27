2d7886587224ab71deb62c6ea7126f3249bd86da
rafadlis <rahmatfadlis@gmail.com>
2025-07-27 08:47:48 +0700

    fix(logo): enhance logo component styling with font-medium class

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <div className="size-6 bg-foreground text-background text-center rounded-sm">
+    <div className="size-6 bg-foreground text-background text-center rounded-sm font-medium">
       r.
     </div>
   )
